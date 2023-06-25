484984beb2efc71cb2495daf4f6eb3899799c3c8
femi <abimbola_mj@yahoo.com>
2023-06-25 22:13:08 +0100

    working on hero-3

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,12 @@
 import styles from '../styles.js';
 import { discount, robot } from '../assets';
+import GetStarted from './GetStarted'
 
 const Hero = () => {
   return (
     <section id='home' className={`flex flex-col md:flex-row ${styles.paddingY}`}>
       <div className={`flex-1 flex-col ${styles.flexStart} px-6 sm:px-16`}>
+        {/* The div for the 20 ... */}
         <div className={`flex flex-row items-center py-[6px] bg-discount-gradient px-4 mb-2 rounded-[10px]`}> 
           <img scr={discount} alt='discount' className='w-[32px] h-[32px]'/>
           <p className={`${styles. paragraph} ml-2`}>
@@ -15,10 +17,12 @@ const Hero = () => {
             </p>
         </div>
         <div className='flex flex-row justify-between items-center w-full'>
-          <h1 className={`flex-1 font-poppins font-semibold ss:text-[72px] text-white text-[52px]`}> The Next <br className='sm:block hidden'/> {' '} 
+          <h1 className={`flex-1 font-poppins font-semibold ss:leading=[70px] ss:text-[72px] text-white text-[52px]`}> The Next <br className='sm:block hidden'/> {' '} 
           <span className='text-gradient'> Generation </span> <br/> Payment method 
           </h1>
-
+          <div className='ss:flex hidden md:mr-4 mr-0'> 
+              <GetStarted />
+          </div>
         </div>
       </div>
     </section>
